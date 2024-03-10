6507ee278dfd9b3bc1748461912319e37db6a42d
Sergio Orejarena <sergioorueda@gmail.com>
2024-03-10 18:18:02 -0500

    ServerApp testing added

--- tests/src/app.test.ts
+++ tests/src/app.test.ts
@@ -1,6 +1,29 @@
 
-describe('App', () => {
-    test('Should be true', () => {
-        expect(true).toBe(true);
+import {ServerApp} from "../../src/presentation";
+
+describe('Test App.ts', () => {
+
+    test('Should call Server.run with values', async () => {
+
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+    process.argv = [
+        'node', 'app.ts',
+        '-b', '4',
+        '-l', '15',
+        '-s',
+        '-n', 'testing-app',
+        '-d', 'test-path',
+    ];
+
+    await import ('../../src/app');
+
+    expect( serverRunMock ).toHaveBeenCalledWith({
+        base: 4,
+        limit: 15,
+        showTable: true,
+        fileName: 'testing-app',
+        filePath: 'test-path',
+    });
     });
 });
